da22133c37db1083cfddeb6b801d30b49adefeff
Braian Troncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-07-19 00:42:35 -0300

    Add files via upload
    Positivo o negativo en python

---
+++ positive_or_negative_number.py
@@ -0,0 +1,9 @@
+numIf = 0
+
+if (numIf < 0):
+    print("Es un numero negativo")
+elif (numIf > 0):
+    print("Es un numero positivo")
+elif (numIf == 0):
+    print("Es un 0")
+else:print("El valor ingresado no es valido")
