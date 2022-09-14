faf9c4875d404c29f3d1d24545a60a2eed7a72bf
Braian Troncoso <95662710+BraianTroncoso@users.noreply.github.com>
2022-09-14 14:50:31 -0300

    gameHistory
    Juego de historia bizzarra

---
+++ historiaBizzarra.py
@@ -0,0 +1,19 @@
+año = int(input('Ingrese un año '))
+nombre = input('Ingrese un nombre ')
+objt = input('Ingrese un objeto ')
+verbo = input('Ingrese un verbo ')
+nombre1 = input('Ingrese otro nombre ')
+adj = input(f'Ingrese un adjetivo para {nombre1} ')
+poder = input('Ingrese un poder ')
+print("")
+print("")
+print("******************************************************************************************************")
+print("******************************H I S T O R I A  B I Z A R R A******************************************")
+print("******************************************************************************************************")
+print("")
+
+print(
+    f"Corria el año {año},cuando el señor {nombre} caminaba por el campo encontró un {objt} ,\n"
+    f"no lo podía creer y decidió {verbo} a su casa para mostrarselo a {nombre1}. {nombre1} \n"
+    f"era una persona con aspecto de {adj}, lo que nadie sabia es que tenia un super poder oculto el cual era {poder}\n"
+    f"al ver que {nombre} encontró un {objt}, {nombre1} decide mostrarle su habilidad oculta que es {poder}")
